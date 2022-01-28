79c362e62ed0a5388361788db0766c375600bc4b
Full Teach Academy <98331260+fullteach@users.noreply.github.com>
2022-01-28 13:33:58 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+# print - bu dastur natijasini ko'rsatadi
+# print(5+5)
+# print("Hello world")
+# str -String - bu yozuvli malumot turi
+# print("Python dasturlash tili")
+# int - Integer -bu sonli malumot turi
+# print(8*9)
+# print(8+15)
+# print("Salom "+'dunyo')
+# print(5+'Python') - ikki xil turdagi malumotni qoshib bolmaydi
+# * + - / ** // %
+# print(5**3) - ** darajani ifodalaydi
+# print(5//3)  - // butun bo'lish
+# print(5%3) - % qoldiqli bo'lish
+# a='Behzod'
+# b=10
+# print(a)
+# a=b=c=0
+
+# a,b,c=1,2,3
+# print(a,b,c)
+# Functions Methods Objects
+# print()
+# print(type('Python'))
+# print(type(5))
+# print('BEHZOD'.lower())
+# a=10
+# b='10'
+# # b=int(b)
+# a=str(a)
+# print(type(a),type(b))
+# a=input("Ismingizni kiriting: ")
+# print("Salom ",a)
+
+# Foydalanuvchi yoshi kiritiladi uni qachon tug’ilgan ekanligini
+# ko’rsatuchi dastur tayyorlash.
+
+
+
+
+
